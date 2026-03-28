b19b37904029282c93337d984278ed871f06ca85
prriyaldesai-prog <prriyaldesai@gmail.com>
2026-03-28 14:44:04 +0530

    multidimentionalarray

---
+++ MultidimentionalArray.js
@@ -0,0 +1,19 @@
+let n1 = 5;
+for (let i = 1; i <= n1; i++) {
+    let row = " ";
+    for (let j = 1; j <= i; j++) {
+        row += "* ";
+    }
+    console.log(row.trim());
+}
+
+//
+let n = 3;
+for (let i = 1; i <= n; i++) {
+    let row = " ";
+    for (let j = 1; j <= i; j++) {
+        //row = row + "* ";
+        row += "* ";
+    }
+    console.log(row.trim());
+}
